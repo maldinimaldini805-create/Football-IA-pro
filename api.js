c8c9a119c877a5326421dba9401b475519a22a05
maldinimaldini805-create <maldinimaldini805@gmail.com>
2026-07-26 21:46:37 +0000

    Update api.js

--- api.js
+++ api.js
@@ -1,1 +1,18 @@
+const URL_API = "https://football-api.maldinimaldini805-4cd.workers.dev";
 
+async function recupererMatchs() {
+    try {
+        const reponse = await fetch(URL_API + "/matches");
+
+        if (!reponse.ok) {
+            throw new Error("Erreur lors de la récupération des matchs.");
+        }
+
+        const donnees = await reponse.json();
+        return donnees;
+
+    } catch (erreur) {
+        console.error(erreur);
+        return null;
+    }
+}
