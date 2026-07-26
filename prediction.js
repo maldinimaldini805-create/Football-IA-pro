480688dddedf26b47655bebc0195e9ad15c96941
maldinimaldini805-create <maldinimaldini805@gmail.com>
2026-07-26 12:14:30 +0000

    Ajout de la fonction analyserMatch dans prediction.js

--- prediction.js
+++ prediction.js
@@ -1,2 +1,31 @@
-# Football-IA-pro
-Football IA pro
+function analyserMatch(donnees) {
+
+    return {
+
+        victoireDomicile: 45,
+
+        matchNul: 28,
+
+        victoireExterieur: 27,
+
+        scoreProbable: "2 - 1",
+
+        confiance: 78,
+
+        buts: 3,
+
+        tirs: 16,
+
+        tirsCadres: 7,
+
+        corners: 9,
+
+        cartons: 4,
+
+        fautes: 23,
+
+        possession: "54% - 46%"
+
+    };
+
+}
