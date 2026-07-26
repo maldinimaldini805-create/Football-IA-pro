267c240ec06478d43479ca3cc75d60f6f9fc14d7
maldinimaldini805-create <maldinimaldini805@gmail.com>
2026-07-26 12:25:04 +0000

    Ajout de calculerStatistiques dans statistiques.js

--- statistiques.js
+++ statistiques.js
@@ -1,1 +1,25 @@
+function calculerStatistiques(data) {
 
+    return {
+
+        forme: data.forme || "N/A",
+
+        butsMarques: data.butsMarques || 0,
+
+        butsEncaisses: data.butsEncaisses || 0,
+
+        tirs: data.tirs || 0,
+
+        tirsCadres: data.tirsCadres || 0,
+
+        corners: data.corners || 0,
+
+        cartonsJaunes: data.cartonsJaunes || 0,
+
+        fautes: data.fautes || 0,
+
+        possession: data.possession || "0%"
+
+    };
+
+}
