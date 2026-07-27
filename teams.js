4cc51cf25e38575c6396317dd4c1613c14680ad5
maldinimaldini805-create <maldinimaldini805@gmail.com>
2026-07-27 13:47:29 +0000

    Create teams.js

---
+++ teams.js
@@ -0,0 +1,56 @@
+// ================================
+// FOOTBALL AI PRO 2.1
+// BASE DES ÉQUIPES
+// ================================
+
+const TEAMS = [
+
+{
+nom:"PSG",
+ligue:"Ligue 1",
+attaque:92,
+defense:88,
+forme:91
+},
+
+{
+nom:"Marseille",
+ligue:"Ligue 1",
+attaque:83,
+defense:80,
+forme:82
+},
+
+{
+nom:"Real Madrid",
+ligue:"LaLiga",
+attaque:95,
+defense:91,
+forme:94
+},
+
+{
+nom:"Barcelona",
+ligue:"LaLiga",
+attaque:91,
+defense:87,
+forme:89
+},
+
+{
+nom:"Manchester City",
+ligue:"Premier League",
+attaque:96,
+defense:92,
+forme:95
+},
+
+{
+nom:"Liverpool",
+ligue:"Premier League",
+attaque:93,
+defense:88,
+forme:91
+}
+
+];
