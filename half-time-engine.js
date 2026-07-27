5bc04f5a4c5298a876ba4f3605a99bd130d2fc12
maldinimaldini805-create <maldinimaldini805@gmail.com>
2026-07-27 18:55:59 +0000

    Create half-time-engine.js

---
+++ half-time-engine.js
@@ -0,0 +1,30 @@
+// ======================================
+// FOOTBALL AI PRO 2.1
+// HALF TIME ENGINE
+// ======================================
+
+function predictionPremiereMiTemps(home, away) {
+
+    return {
+
+        score: "1 - 0",
+
+        confiance: 76,
+
+        premierBut: home.nom,
+
+        minutePremierBut: 27,
+
+        butsPremiereMiTemps: 1,
+
+        cornersPremiereMiTemps: 5,
+
+        cartonsPremiereMiTemps: 2,
+
+        tirsPremiereMiTemps: 11,
+
+        tirsCadresPremiereMiTemps: 4
+
+    };
+
+}
