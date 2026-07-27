180f9030a0fafd20712dd28e5e7f97bd39fad27b
maldinimaldini805-create <maldinimaldini805@gmail.com>
2026-07-27 13:56:14 +0000

    Create event-engine.js

---
+++ event-engine.js
@@ -0,0 +1,34 @@
+// =====================================
+// FOOTBALL AI PRO 2.1
+// EVENT ENGINE
+// =====================================
+
+function predireEvenements(home, away) {
+
+    return {
+
+        premierBut: home.nom,
+
+        premierTir: home.nom,
+
+        premierTirCadre: home.nom,
+
+        premierCorner: home.nom,
+
+        premiereTouche: away.nom,
+
+        premierDegagement: away.nom,
+
+        premiereFaute: away.nom,
+
+        premierCartonJaune: away.nom,
+
+        premierCartonRouge: away.nom,
+
+        premierHorsJeu: home.nom,
+
+        premierArretGardien: away.nom
+
+    };
+
+}
