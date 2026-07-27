6eec79db04d1de953af8a28453c544ab67d667d1
maldinimaldini805-create <maldinimaldini805@gmail.com>
2026-07-27 13:44:35 +0000

    Update live.js

--- live.js
+++ live.js
@@ -1,1 +1,26 @@
+// ===========================
+// FOOTBALL AI PRO 2.1
+// MODE LIVE IA
+// ===========================
 
+function afficherEvenementsRapides(resultat) {
+
+    return [
+
+        "🥅 Premier tir cadré : " + resultat.premierTirCadre,
+
+        "🔄 Première touche : " + resultat.premiereTouche,
+
+        "🦶 Premier dégagement : " + resultat.premierDegagement,
+
+        "🚩 Premier corner : " + resultat.premierCorner,
+
+        "⚽ Premier but : " + resultat.premierBut,
+
+        "❌ Première faute : " + resultat.premiereFaute,
+
+        "🟨 Premier carton : " + resultat.premierCarton
+
+    ];
+
+}
